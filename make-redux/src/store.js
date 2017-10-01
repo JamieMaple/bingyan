a1b6f3c3492eabb5f8554cb074c2d0a289426c3d
JamieMaple <jamiemaple007@gmail.com>
2017-10-01 18:29:24 +0800

    add store

---
+++ make-redux/src/store.js
@@ -0,0 +1,61 @@
+let appState = {
+  title: {
+    text: 'React.js 小书',
+    color: 'red',
+  },
+  content: {
+    text: 'React.js 小书内容',
+    color: 'blue'
+  }
+}
+
+function renderApp(appState) {
+  console.log('render app...')
+  renderTitle(appState.title)
+  renderContent(appState.content)
+}
+function renderTitle(title) {
+  console.log('render title...')
+  const titleDom = document.getElementById('title')
+  titleDom.innerHTML = title.text
+  titleDom.style.color = title.color
+}
+function renderContent(content) {
+  console.log('render content...')
+  const contentDom = document.getElementById('content')
+  contentDom.innerHTML = content.text
+  contentDom.style.color = content.color
+}
+
+function stateChanger(state, action) {
+  switch (action.type) {
+  case 'UPDATE_TITLE_TEXT':
+    appState.title.text = action.text
+    break
+  case 'UPDATE_TITLE_COLOR':
+    appState.title.color = action.color
+    break
+  default:
+    break
+  }
+}
+function createStore(state, stateChanger) {
+  const getState = () => state
+  const listeners = []
+  const subscribe = (listener) => (listeners.push(listener))
+  const dispatch = (action) => {
+    stateChanger(state, action),
+    listeners.forEach((listener) => listener())
+  }
+
+  return { getState, dispatch, subscribe }
+}
+
+const store = createStore(appState, stateChanger)
+
+store.subscribe(() => (renderApp(store.getState())))
+renderApp(store.getState())
+
+store.dispatch({type: 'UPDATE_TITLE_TEXT', text: 'changed'})
+store.dispatch({type: 'UPDATE_TITLE_COLOR', color: 'green'})
+
