c741948d7b4db44567495468983234d6d55f5a54
JamieMaple <jamiemaple007@gmail.com>
2017-10-02 20:25:25 +0800

    change food

--- nodePaChong/food.js
+++ nodePaChong/food.js
@@ -7,23 +7,21 @@ request = install(request)
 // 分类 --- 零食类
 var items = []
 
-Promise.resolve().then(
-  function() {
-    request.get('http://store.dangdang.com/531')
-    .charset('gbk')
-    .end(function(err, sres) {
-      var $ = cheerio.load(sres.text)
-      $('.products li').each(function(index, element) {
-        var name = $(this).find('.name a').text().substr(7)
-        var discribe = ""
-        var price = Number($(this).find('.num').text()+$(this).find('.tail').text())
-        var img = $(this).find('.img img').attr('data-original')
-        var category = 0
-        items.push({
-          name, price, discribe, img, category
-        })
+function addItems(num) {
+  request.get('http://store.dangdang.com/531?page_index='+num)
+  .charset('gbk')
+  .end(function(err, sres) {
+    var $ = cheerio.load(sres.text)
+    $('.products li').each(function(index, element) {
+      var name = $(this).find('.name a').text().substr(7)
+      var discribe = ""
+      var price = Number($(this).find('.num').text()+$(this).find('.tail').text())
+      var img = $(this).find('.img img').attr('data-original')
+      var category = 0
+      items.push({
+        name, price, discribe, img, category
       })
-      console.log(items)
-   })
-  }
-)
+    })
+ })
+}
+
