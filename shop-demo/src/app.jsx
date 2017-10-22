95ef5985528088551cb704acbd8de9f5ac501598
JamieMaple <jamiemaple007@gmail.com>
2017-10-22 20:15:32 +0800

    demo

--- shop-demo/src/app.jsx
+++ shop-demo/src/app.jsx
@@ -1,13 +1,10 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
-import { TransitionGroup } from 'react-transition-group'
 
 import IndexPage from './pages/index'
 import MainPage from './pages/mainPage'
 import SignIn from './pages/signin'
 import SignUp from './pages/signup'
-
-import AnimateTransition from './components/AnimateTransition'
 
 const style = {}
 style.routeWrapper = {
@@ -18,27 +15,12 @@ style.routeWrapper = {
 }
 
 const App = () => (
-  <Route render={({location}) => (
-    <TransitionGroup>
-      <AnimateTransition
-        key={location.pathname}
-        classNames="page-fade"
-        timeout={300}
-      >
-        <div
-          className="route-wrapper" 
-          style={style.routeWrapper}
-        >
-          <Switch>
-            <Route exact path='/' component={IndexPage} />
-            <Route exact path='/signin' component={SignIn} />
-            <Route exact path='/signup' component={SignUp} />
-            <Route path='/:others' component={MainPage} />
-          </Switch>
-        </div>
-      </AnimateTransition>
-    </TransitionGroup>
-  )} />
+  <Switch>
+    <Route exact path='/' component={IndexPage} />
+    <Route exact path='/signin' component={SignIn} />
+    <Route exact path='/signup' component={SignUp} />
+    <Route path='/:others' component={MainPage} />
+  </Switch>
 )
 
 export default App
